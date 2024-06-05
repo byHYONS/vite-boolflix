db1d54171c91cfb4daa48f2cc9cf434b21c2f5a8
byHYONS <byhyons@gmail.com>
2024-06-05 13:07:41 +0200

    add apiInfo

--- src/store.js
+++ src/store.js
@@ -2,7 +2,18 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    text: 'Hello World!'
+    moveRisults: [],
+    tvRisults: [],
+    apiInfo: {
+        url: 'https://api.themoviedb.org/3/',
+        queryMove: 'search/movie?',
+        queryTv: 'search/tv?',
+        apiKey: 'e15053208c9a5cbb8be0e4a844fc6700',
+        apiToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMTUwNTMyMDhjOWE1Y2JiOGJlMGU0YTg0NGZjNjcwMCIsInN1YiI6IjY2NjAyZjIyNDI5NjM4OTY3YzY5MTc2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KuHygX9Ee7rci6fe7CFGamLrMmAOq1Ir5nQqP3DzDX4',
+
+    }
+
     
 
 })
+
