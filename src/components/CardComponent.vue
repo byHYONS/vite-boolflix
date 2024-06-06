c3d00b1e11f6dc6ac844ea8db7cee22bf1463682
byHYONS <byhyons@gmail.com>
2024-06-06 14:57:44 +0200

    added flags for language

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -27,8 +27,23 @@ export default {
 
         <p v-show="title.length"> <strong>Titolo:</strong> {{ title }} </p>
         <p> <strong>Titolo originale:</strong> {{ titleOrig }} </p>
-        <p> <strong>lingua:</strong> {{ language }} </p>
         <p> <strong>Voto:</strong> {{ vote }} </p>
+
+        <div v-if="language === 'it'">
+            <strong>lingua: </strong> 
+            <img src="../../public/flags/it.svg" alt="language">
+        </div>
+        <div v-else-if="language === 'en'">
+            <strong>lingua: </strong> 
+            <img src="../../public/flags/us.svg" alt="language">
+        </div>
+        <div v-else-if="language === 'al'">
+            <strong>lingua: </strong> 
+            <img src="../../public/flags/al.svg" alt="language">
+        </div>
+        <div v-else> 
+            <strong>lingua:</strong> {{ language }} 
+        </div>
 
     </div>
 
@@ -43,6 +58,11 @@ export default {
 .card {
     height: 150px;
     width: 70px;
+    img {
+        margin-left: .3125rem;
+        width: 1.375rem;
+        vertical-align: middle;
+    }
 }
 
 </style>
